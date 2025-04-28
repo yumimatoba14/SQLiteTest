90099b022c88c772018276a6adde60a64086271c
yumimatoba14 <yumimatoba14@gmail.com>
2025-04-28 18:09:09 +0900

    added minimum codes. - added DbContext and an entity class. - added some necessary packages. -- Microsoft.EntityFrameworkCore.Sqlite -- Microsoft.EntityFrameworkCore.Tools

---
+++ SQLiteTest/Models/WfDbContext.cs
@@ -0,0 +1,27 @@
+﻿//using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace SQLiteTest.Models
+{
+    public class WfDbContext : DbContext
+    {
+        public DbSet<Workflow> Workflows { get; internal set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            // プロジェクト直下にデータベースファイルを配置するため、モジュールのパス起点のフルパスを用いる。
+            // Update-Database はソリューションフォルダで実行されるのに対し、モジュールのデバッグ実行ではデフォルトでは出力フォルダを作業フォルダとして
+            // 実行してしまうようで、相対パスでは同じ場所を指定することができない。
+            string modulePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string filePath = System.IO.Path.Combine(modulePath, "../../../workflow.sqlite3");
+            string connectionString = new SqliteConnectionStringBuilder { DataSource = filePath }.ToString();
+            optionBuilder.UseSqlite(new SqliteConnection(connectionString));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
--- SQLiteTest/Program.cs
+++ SQLiteTest/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using SQLiteTest.Models;
 
 namespace SQLiteTest
 {
@@ -10,6 +8,30 @@ namespace SQLiteTest
     {
         static void Main(string[] args)
         {
+            using (var db = new WfDbContext())
+            {
+                if (db.Workflows.Count() == 0)
+                {
+                    var workflow = new Workflow { Id = 1, Name = "Workflow1" };
+                    db.Workflows.Add(workflow);
+
+                    db.SaveChanges();
+                    Console.WriteLine("Data has been added.");
+                }
+            }
+            using (var db = new WfDbContext())
+            {
+                int id = 1;
+                var workflow = db.Workflows.Find(id);
+                if (workflow != null)
+                {
+                    Console.WriteLine("workflow.Name = {0}", workflow.Name);
+                }
+                else
+                {
+                    Console.WriteLine("Workflow(Id = {0}) is not found.", id);
+                }
+            }
         }
     }
 }
